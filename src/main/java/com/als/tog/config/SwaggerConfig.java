c1330836cebeb07e72e47055d34bc091b6882a99
dou kaiwei <2200600836@qq.com>
2024-04-09 14:28:12 +0800

    init

--- src/main/java/com/als/tog/config/SwaggerConfig.java
+++ src/main/java/com/als/tog/config/SwaggerConfig.java
@@ -1,41 +1,119 @@
-//package com.als.tog.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//
-//@Configuration // 标明是配置类
-//@EnableSwagger2 //开启swagger功能
-//public class SwaggerConfig {
-//    @Bean
-//    public Docket createRestApi() {
-//        return new Docket(DocumentationType.SWAGGER_2)  // DocumentationType.SWAGGER_2 固定的，代表swagger2
-////                .groupName("分布式任务系统") // 如果配置多个文档的时候，那么需要配置groupName来分组标识
-//                .apiInfo(apiInfo()) // 用于生成API信息
-//                .select() // select()函数返回一个ApiSelectorBuilder实例,用来控制接口被swagger做成文档
-//                .apis(RequestHandlerSelectors.basePackage("com.als.tog.web")) // 用于指定扫描哪个包下的接口
-//                .paths(PathSelectors.any())// 选择所有的API,如果你想只为部分API生成文档，可以配置这里
-//                .build();
-//    }
-//
-//    /**
-//     * 用于定义API主界面的信息，比如可以声明所有的API的总标题、描述、版本
-//     * @return
-//     */
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder()
-//                .title("XX项目API") //  可以用来自定义API的主标题
-//                .description("XX项目SwaggerAPI管理") // 可以用来描述整体的API
-//                .termsOfServiceUrl("") // 用于定义服务的域名
-//                .version("1.0") // 可以用来定义版本。
-//                .build(); //
-//    }
-//}
-//
+package com.als.tog.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.*;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+import springfox.documentation.builders.*;
+import springfox.documentation.schema.ScalarType;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ *
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Value("${swagger.application.name:dkw}")
+    private String applicationName;
+
+    @Value("${swagger.application.enable:true}")
+    private boolean enable;
+
+    @Bean
+    public Docket createRestApi() {
+        List<RequestParameter> parameters = new ArrayList<>();
+        parameters.add(new RequestParameterBuilder()
+                .query(simpleParameterSpecificationBuilder -> simpleParameterSpecificationBuilder
+                        .model(modelSpecificationBuilder -> modelSpecificationBuilder.scalarModel(ScalarType.STRING))
+                        .defaultValue(""))
+                .in(ParameterType.HEADER)
+                .name("token")
+                .description("令牌")
+                .required(true)
+                .build());
+        parameters.add(new RequestParameterBuilder()
+                .in(ParameterType.HEADER)
+                .query(simpleParameterSpecificationBuilder -> simpleParameterSpecificationBuilder
+                        .model(modelSpecificationBuilder -> modelSpecificationBuilder.scalarModel(ScalarType.STRING))
+                        .defaultValue("")).name("curOrgCode")
+                .description("当前组织编码")
+                .required(true).build());
+        parameters.add(new RequestParameterBuilder()
+                .in(ParameterType.HEADER)
+                .name("curDeptCode")
+                .query(simpleParameterSpecificationBuilder -> simpleParameterSpecificationBuilder
+                        .model(modelSpecificationBuilder -> modelSpecificationBuilder.scalarModel(ScalarType.STRING))
+                        .defaultValue(""))
+                .description("当前部门编码")
+                .required(true)
+                .build());
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .enable(enable)
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
+                .select()
+                //这里根据你们实际文件目录修改
+                .apis(RequestHandlerSelectors.basePackage("com.als.tog.web"))
+                .paths(PathSelectors.any())
+                .build().globalRequestParameters(parameters);
+    }
+
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(applicationName + "接口文档")
+                .version("1.0")
+                .build();
+    }
+
+    /**
+     * 处理actuator走了pathPatternsCondition，使用ant_path_matcher
+     * @param webEndpointsSupplier
+     * @param servletEndpointsSupplier
+     * @param controllerEndpointsSupplier
+     * @param endpointMediaTypes
+     * @param corsProperties
+     * @param webEndpointProperties
+     * @param environment
+     * @return
+     */
+    @Bean
+    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier,
+                                                                         ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier,
+                                                                         EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties,
+                                                                         WebEndpointProperties webEndpointProperties, Environment environment) {
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList<>();
+        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+        String basePath = webEndpointProperties.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping =
+                webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath)
+                        || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
+        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes,
+                corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath),
+                shouldRegisterLinksMapping, null);
+    }
+}
